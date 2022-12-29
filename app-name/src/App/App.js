f049bbf4c959d412f1793d9033a1ba6e6d833c39
loboda.rom@gmail.com <loboda.rom@gmail.com>
2022-12-29 12:34:30 +0200

    addButtons&TimeLessCollor

--- app-name/src/App/App.js
+++ app-name/src/App/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Timer name = "First" hours="1" minutes="0" seconds="10"></Timer>
       <Timer name = "Second" hours="0" minutes="10" seconds="10"></Timer>
-      <Timer name = "Third" hours="0" minutes="0" seconds="10"></Timer>
+      <Timer name = "Third" hours="0" minutes="0" seconds="40"></Timer>
     </div>
   );
 }
--- app-name/src/AppTime/AppTime.js
+++ app-name/src/AppTime/AppTime.js
@@ -4,10 +4,15 @@ import React, {useState, useEffect} from "react";
 const Timer = (props) =>{
 
     const [[hr, min, sec,ms], setTime] = useState([props.hours, props.minutes, props.seconds, 998]);
-
+    const [timeLess,timeLessChange] = useState(false);
+    const [isCounting,setIsCounting] = useState(true);
+    
     const timerTick = () => {
+        if(hr == 0 && min == 0 && sec == 30 && ms == 0){
+            timeLessChange(true);
+        }
         if (hr <= 0 && min <= 0 && sec <= 0 && ms <= 0){
-            clearInterval(this);
+            clearInterval(this.timer);
         }
         else if (min <= 0 && sec <= 0 && ms <= 0) {
             setTime([hr - 1, 59, 59, 998]);
@@ -17,14 +22,22 @@ const Timer = (props) =>{
         }
         else if (ms <= 0) {
           setTime([hr, min, sec-1, 998]);
-      }
+       }
         else {
             setTime([hr, min, sec, ms-2]);
         }      
-    };  
-
+    };
+      
+    function Restart(){
+        setTime([props.hours, props.minutes, props.seconds, 998]);
+        timeLessChange(false);
+    }
+    
     useEffect (() =>{
-        const timer = setInterval(() => timerTick(), 1);
+        const timer = setInterval(() => {
+            isCounting &&
+            timerTick()
+        }, 1);
         return () => {
             clearInterval(timer);
         }
@@ -33,10 +46,15 @@ const Timer = (props) =>{
     return(
       <div className="container"> 
         <h4>Timer - {props.name} :</h4>
-        <div className='div__time'>{`${hr.toString().padStart(2, '0')}`} :</div>
+        <div style={{color: !timeLess ? "white" : "red"}}>
+          <div className='div__time'>{`${hr.toString().padStart(2, '0')}`} :</div>
           <div className='div__time'> {`${min.toString().padStart(2, '0')}`} :</div>
           <div className='div__time'> {`${sec.toString().padStart(2, '0')}`} :</div>
           <div className='div__time'> {`${ms.toString().padStart(3,'0')}`}</div>
+        </div>
+        {isCounting ? (<button className='btn' onClick={() => setIsCounting(false)}>Pause</button>):
+        (<button className='btn' onClick={() => setIsCounting(true)}>Start</button>)}
+          <button className='btn' onClick={Restart}>Restart</button>
       </div>
     );
 };
